f72162f61e4774e411f2a9e015ea8d08871df6f6
Travis Gui <maboite41@gmail.com>
2021-02-16 17:04:53 +0100

    port du lancement du serveur spécifié

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,19 @@
 
 const http = require('http');
 const app = require('./app');
+
+/*la fonction normalizePort renvoie un port valide, qu'il soit fourni 
+sous la forme d'un numéro ou d'une chaîne*/
+const normalizePort = val => {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
