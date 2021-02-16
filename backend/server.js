53ec61579372933cd86bff76869575138858300a
Travis Gui <maboite41@gmail.com>
2021-02-16 17:23:05 +0100

    commentaire ajouté

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const normalizePort = val => {
   return false;
 };
 const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+//app.set('port', port);
 
 
 //la fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur
