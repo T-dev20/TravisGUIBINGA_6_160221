faa03d835fa70f0453fcf26c77846a65d2e95eb3
Travis Gui <maboite41@gmail.com>
2021-02-17 16:37:11 +0100

    routeur sauces implémenté

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -6,10 +6,10 @@ const multer = require('../middleware/multer-config');
 
 const saucesCtrl = require('../controllers/sauces');
 
-router.get('/', auth, saucesCtrl.getAllStuff);
-router.post('/', auth, multer, saucesCtrl.createThing);
-router.get('/:id', auth, saucesCtrl.getOneThing);
-router.put('/:id', auth, multer, saucesCtrl.modifyThing);
-router.delete('/:id', auth, saucesCtrl.deleteThing);
+router.get('/', auth, saucesCtrl.getAllSauce);
+router.post('/', auth, multer, saucesCtrl.createSauce);
+router.get('/:id', auth, saucesCtrl.getOneSauce);
+router.put('/:id', auth, multer, saucesCtrl.modifySauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
 
 module.exports = router;
