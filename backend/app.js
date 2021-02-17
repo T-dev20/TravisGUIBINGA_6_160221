77bb73ba6d3bdf771e875976abd98ef7ac6e6097
Travis Gui <maboite41@gmail.com>
2021-02-17 11:59:44 +0100

    implémentation du CORS

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,19 @@ mongoose.connect('mongodb+srv://new-user_01:CRVrLEKDFEoKKfXR@cluster0.lzdtw.mong
 const app = express();
 
 
+/*** 1)On accède à notre API depuis n'importe quelle origine
+2)On ajoute les headers mentionnés aux requêtes envoyées vers notre API 
+3)On envoie des requêtes avec les méthodes mentionnées ***/
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+
 /*** Exportation de l'app pour pouvoir y accéder depuis les 
 autres fichiers du projet, notamment notre serveur Node ***/
 module.exports = app;
