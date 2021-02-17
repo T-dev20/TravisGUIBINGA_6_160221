cb5d644bfe5d8e29b1dd0c536c3d288447aa6eef
Travis Gui <maboite41@gmail.com>
2021-02-17 11:23:26 +0100

    connexion à la base de données implémenté

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,14 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://new-user_01:CRVrLEKDFEoKKfXR@cluster0.lzdtw.mongodb.net/SoPekockoDB?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 
 /*** Exportation de l'app pour pouvoir y accéder depuis les 
 autres fichiers du projet, notamment notre serveur Node ***/
