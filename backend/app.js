ec1d76549372846fce4994b53808ff579ff58ee6
Travis Gui <maboite41@gmail.com>
2021-02-17 11:53:24 +0100

    import du bodyParser

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 //importation de Express
 const express = require('express');
-
-const app = express();
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 
@@ -10,6 +9,11 @@ mongoose.connect('mongodb+srv://new-user_01:CRVrLEKDFEoKKfXR@cluster0.lzdtw.mong
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+/****Notre application sera contenue dans cette constante
+ la méthode express() permet de créer une app Express*****/
+const app = express();
 
 
 /*** Exportation de l'app pour pouvoir y accéder depuis les 
