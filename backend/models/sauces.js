d54de01e00a78a3515a563ab97697fcdd10abfc1
Travis Gui <maboite41@gmail.com>
2021-02-18 13:09:20 +0100

    modèle sauses modifié

--- backend/models/sauces.js
+++ backend/models/sauces.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
+  userId: { type: String, unique: true },
   name: { type: String, required: true },
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
-  imageUrl: { type: String, required: true },
-  heat: { type: Number, required: true },
-  likes: { type: Number, required: true },
-  dislikes: { type: Number, required: true },
-  usersLiked: { type: [String], required: true },
-  name: { type: [String], required: true },
+  manufacturer: { type: String },
+  description: { type: String },
+  mainPepper: { type: String },
+  imageUrl: { type: String },
+  heat: { type: Number },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  usersLiked: { type: Array, default: [] },
+  usersDisliked: { type: Array, default: [] },
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
