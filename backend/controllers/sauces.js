b89df850e52b84eb40ff076c60830208a7e45569
Travis Gui <maboite41@gmail.com>
2021-02-20 15:20:12 +0100

    controlleur sauces : commentaires modifiés

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,18 +1,13 @@
 const Sauce = require('../models/sauces');
 const fs = require('fs');
 
-// Pour ajouter un fichier à la requête, le front-end doit envoyer les 
-// données de la requête sous la forme form-data, et non sous forme de JSON. 
-
-// Nous devons également résoudre l'URL complète de notre image, car 
-// req.file.filename ne contient que le segment filename . Nous utilisons 
-// req.protocol pour obtenir le premier segment (dans notre cas 'http' ).
+// Controlleur de création d'une sauce
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
   const sauce = new Sauce({
     ...sauceObject,
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`  //Nous utilisons req.protocol pour obtenir le premier segment (dans notre cas 'http' ).
   });
   sauce.save()
     .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
@@ -29,52 +24,33 @@ exports.createSauce = (req, res, next) => {
     .then(() => res.status(201).json({ message: 'Sauce aimée !'}))
     .catch(error => res.status(400).json({ error }));
 };*/
-
+// Controlleur d'affichage d'une sauce
 exports.getOneSauce = (req, res, next) => {
-  Sauce.findOne({
-    _id: req.params.id
-  }).then(
-    (sauce) => {
-      res.status(200).json(sauce);
-    }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      });
-    }
-  );
+    Sauce.findOne({_id: req.params.id})
+    .then((sauce) => { res.status(200).json(sauce);})
+    .catch((error) => {res.status(404).json({error});
+  });
 };
 
-/*** Dans cette fonction, on crée un objet sauceObject 
-qui regarde si req.file existe ou non. S'il existe, on traite la nouvelle
-image ; s'il n'existe pas, on traite simplement l'objet entrant. 
-On crée ensuite une instance Sauce à partir de sauceObject , puis on effectue la modification.***/
+// Controlleur de modification d'une sauce
 exports.modifySauce = (req, res, next) => {
-  const sauceObject = req.file ?
+  const sauceObject = req.file ?  //Création de l'objet sauceObject pour vérifier si req.file existe ou non
     {
       ...JSON.parse(req.body.sauce),
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`  // S'il existe, on traite la nouvelle image, sinon on traite simplement l'objet entrant.
     } : { ...req.body };
-  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })  //On crée ensuite une instance Sauce à partir de sauceObject , puis on effectue la modification.
     .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
     .catch(error => res.status(400).json({ error }));
 };
 
 
-/*** nous utilisons le fait de savoir que notre URL d'image contient un 
-segment /images/ pour séparer le nom de fichier ;
-
- nous utilisons ensuite la fonction unlink du package fs pour supprimer 
- ce fichier, en lui passant le fichier à supprimer et le callback à exécuter une fois ce fichier supprimé ;
-
-dans le callback, nous implémentons la logique d'origine, en supprimant 
-la sauce de la base de données. ***/
+// Controlleur de suppression d'une sauce
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then(sauce => {
-      const filename = sauce.imageUrl.split('/images/')[1];
-      fs.unlink(`images/${filename}`, () => {
+      const filename = sauce.imageUrl.split('/images/')[1];  //nous utilisons le fait de savoir que notre URL d'image contient un segment /images/ pour séparer le nom de fichier
+      fs.unlink(`images/${filename}`, () => {  //Suppression du fichier et callback à exécuter une fois ce fichier supprimé
         Sauce.deleteOne({ _id: req.params.id })
           .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
           .catch(error => res.status(400).json({ error }));
@@ -83,16 +59,11 @@ exports.deleteSauce = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
+
+// Controlleur d'affichage de toutes les sauces
 exports.getAllSauce = (req, res, next) => {
-  Sauce.find().then(
-    (sauces) => {
-      res.status(200).json(sauces);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+  Sauce.find()
+  .then((sauces) => {res.status(200).json(sauces);})
+  .catch((error) => {res.status(400).json({error});
+  });
 };
