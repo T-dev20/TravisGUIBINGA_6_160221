59ec90cf40ef0e7d7965abb84a0eefa047fff675
Travis Gui <maboite41@gmail.com>
2021-02-20 16:44:24 +0100

    comments add

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
-//importation de Express
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express');  // Importation de Express
+const bodyParser = require('body-parser');  // Importation de body-parser: analysee les corps de requêtes entrants dans un middleware
 //importation des routeurs
 const sauceRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
-const path = require('path');
+const path = require('path');  // Import de path: fournit un moyen de travailler avec les répertoires et les chemins de fichiers.
 
 const mongoose = require('mongoose');
 
@@ -16,14 +15,13 @@ mongoose.connect('mongodb+srv://new-user_01:CRVrLEKDFEoKKfXR@cluster0.lzdtw.mong
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
-/****Notre application sera contenue dans cette constante
- la méthode express() permet de créer une app Express*****/
+/*** Notre application sera contenue dans cette constante. La méthode express() permet de créer une app Express. ***/
 const app = express();
 
 
-/*** 1)On accède à notre API depuis n'importe quelle origine
-2)On ajoute les headers mentionnés aux requêtes envoyées vers notre API 
-3)On envoie des requêtes avec les méthodes mentionnées ***/
+/*** 1)On accède à notre API depuis n'importe quelle origine;
+     2)On ajoute les headers mentionnés aux requêtes envoyées vers notre API;
+     3)On envoie des requêtes avec les méthodes mentionnées. ***/
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -39,6 +37,5 @@ app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', userRoutes);
 app.use('/api/sauces', sauceRoutes);
 
-/*** Exportation de l'app pour pouvoir y accéder depuis les 
-autres fichiers du projet, notamment notre serveur Node ***/
+// Exportation de l'app pour pouvoir y accéder depuis les autres fichiers du projet, notamment notre serveur Node.
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-//importation du package http de Node,permettant de créer un serveur, puis import de l'API
+// Importation du package http de Node,permettant de créer un serveur, puis import de l'API
 const http = require('http');
 const app = require('./app');
 
 const mongoose = require('mongoose');
 
-/*la fonction normalizePort renvoie un port valide, qu'il soit fourni 
-sous la forme d'un numéro ou d'une chaîne*/
+/*** La fonction normalizePort renvoie un port valide, qu'il soit fourni 
+sous la forme d'un numéro ou d'une chaîne ***/
 const normalizePort = val => {
-  const port = parseInt(val, 10);
+  const port = parseInt(val, 10);  // Analyse de l'argument passé, valeur obtenue assigné à une constante "port"
 
-  if (isNaN(port)) {
+  if (isNaN(port)) {               // Si la constante "port" n'est pas un Nombre (isNaN), la fonction renvoie l'argument analysé(val).
     return val;
   }
-  if (port >= 0) {
+  if (port >= 0) {                 // Si la valeur de la constante "port" est supérieur à zéro et donc valide: la fonction renvoie la consante port.
     return port;
   }
   return false;
@@ -21,7 +21,7 @@ const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 
-//la fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur
+// La fonction errorHandler  recherche les différentes erreurs et les gère de manière appropriée. Elle est ensuite enregistrée dans le serveur
 const errorHandler = error => {
   if (error.syscall !== 'listen') {
     throw error;
@@ -42,11 +42,11 @@ const errorHandler = error => {
   }
 };
 
-//Serveur prêt
+// Serveur prêt
 const server = http.createServer(app);
 
 
-//un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console
+// Un écouteur d'évènements est également enregistré, consignant le port ou le canal nommé sur lequel le serveur s'exécute dans la console
 server.on('error', errorHandler);
 server.on('listening', () => {
   const address = server.address();
@@ -54,5 +54,5 @@ server.on('listening', () => {
   console.log('Listening on ' + bind);
 });
 
-//écoute de la requête via un port qui est par defaut le 3000
+// Écoute de la requête via un port (par defaut: 3000)
 server.listen(port);
